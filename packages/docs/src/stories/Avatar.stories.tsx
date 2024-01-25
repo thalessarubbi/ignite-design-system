8941b82cd03f2065ba16a09206cf33b90714f884
Thales Sarubbi <thalessarubbi@gmail.com>
2024-01-25 09:04:24 -0300

    Initial commit

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,27 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Avatar, AvatarProps } from '@inobra-ui/react'
+
+export default {
+  title: 'Data display/Avatar',
+  component: Avatar,
+
+  args: {
+    src: 'https://github.com/thalessarubbi.png',
+    alt: 'Thales Sarubbi',
+  },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
+} as Meta<AvatarProps>
+
+export const Primary: StoryObj<AvatarProps> = {}
+
+export const WithFallback: StoryObj<AvatarProps> = {
+  args: {
+    src: undefined,
+  },
+}
---
+++ packages/docs/src/stories/Box.stories.tsx
@@ -0,0 +1,20 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Box, BoxProps, Text } from '@inobra-ui/react'
+
+export default {
+  title: 'Form/Box',
+  component: Box,
+
+  args: {
+    children: <Text>Testando o elemento Box </Text>,
+  },
+  argTypes: {
+    children: {
+      control: {
+        type: null,
+      },
+    },
+  },
+} as Meta<BoxProps>
+
+export const Primary: StoryObj<BoxProps> = {}
